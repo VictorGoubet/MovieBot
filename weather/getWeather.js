0114a2448f496bb6bb2a3f080eec957cb0bcac41
repl.it user <replituser@example.com>
2021-02-13 09:19:50 +0000

    weather

--- weather/getWeather.js
+++ weather/getWeather.js
@@ -3,14 +3,14 @@ const axios = require ("axios");
 const dateformat = require('dateformat');
 
 const apikey = process.env.weatherToken; 
-let apiurl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
 
 const getWeather = (location) => {
     let today = new Date()
     let yesterday = dateformat(new Date(today.getTime() - 1000*3600*24), "yyyy-m-dd");
     let after_tomorrow = dateformat(new Date(today.getTime() + 2*1000*3600*24), "yyyy-m-dd");
     today = dateformat(today, "yyyy-m-dd");
-    apiurl = apiurl+location+`/${yesterday}/${after_tomorrow}`
+
+    apiurl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${yesterday}/${after_tomorrow}`
     console.log(apiurl)
     return new Promise(async (resolve, reject)=>{
         try{
